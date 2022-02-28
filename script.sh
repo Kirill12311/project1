970e1db072ceddc59b12ccfa32574d250d718482
Kirill12311 <100598970+Kirill12311@users.noreply.github.com>
2022-02-28 17:04:43 +0000

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
-echo "Вы запустили скрипт с именем $script_name"
+echo "Вы запустили скрипт с именем script.sh"
 echo "Разработчик: Водянкин Кирилл"
 echo "Программа для отключения/подключения репозиториев"
 
-echo -n "Продолжить? (y/n) "
-
-read item
-case "$item" in
-    y|Y) echo "Ввели «y», продолжаем..."
-        ;;
-    n|N) echo "Ввели «n», завершаем..."
-        exit 0
-        ;;
-    *) echo "Ничего не ввели. Выполняем действие по умолчанию..."
-        ;;
-esac
+echo "Список репозиториев"
+yum repolist all
+echo "Список включенных репозиториев"
+yum repolist
+while :
+do
+echo "Введите имя отключенного репозитория, который будет включен или включенного, который будет отключен"
+read -r repo
+if !(checkrepo $repolist); then
+	echo "Такого репозитория нет в списке подключенных, отключить?"
+	echo -n "y/n: "
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then
+	    yum-config-manager --disable $repo
+	else
+	    yum-config-manager --enable $repo
+	fi
+	echo "Закончили?"
+	echo -n "y/n: "
+	read answer
+	if [ "$answer" != "${answer#[Nn]}" ] ;then
+	        echo "Выход!"
+	    exit
+	else
+	    echo "Закончили!!"
+	fi
+else
+	echo "Закончили?"
+	echo -n "y/n: "
+	read answer
+	if [ "$answer" != "${answer#[Nn]}" ] ;then
+		echo "Выход!!"
+		exit
+	else
+		echo "Закончили!"
+fi
+fi
+done

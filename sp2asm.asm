2c5df874d4893ff5e0a0311b866b93a404e406de
Kirill12311 <100598970+Kirill12311@users.noreply.github.com>
2022-06-07 14:01:13 +0700

    Add files via upload

--- sp2asm.asm
+++ sp2asm.asm
@@ -1,32 +1,42 @@
-        global _start
-        section .data  
-                    mas dw 10, -2, 3, 3, 2, -7, 4, -6, 0, 0     
-                    len dw $-mas   
-            
-                 
-    section .text     
-        _start:   
-                    
-            mov ax, mas  
-  
-            lea bx, [mas]
-            mov cx, len     
-            xor ax, ax   
-        loop1:   add al, [bx]   
-            inc bx  
-            loop loop1    
-                 
-            aam     
-            add ax, 3030h   
-            mov bx, ax 
-            mov ah, 02   
-            mov dl, bh   
-            int 21h      
-            mov dl, bl   
-            int 21h      
-                 
-             
-        end:
-          mov ax, 4c00h    
-            int 21h   
-        
+global _start
+
+section .data
+arr1 dw 10, -2, 3, 3, 2, -7, 4, -6, 0, 0
+ans dw 0
+
+section .bss
+arr2 resw 10
+
+section .text
+_start:
+	mov ecx, 10
+	mov esi, arr1
+	mov edi, arr2
+	xor eax, eax
+loop1:
+	lodsw
+	bt eax, 15
+	jae loop2
+	not eax
+	add eax, 1
+loop2:
+	stosw
+	loop loop1
+loop3:
+	mov ecx, 10
+	mov esi, arr2
+	xor ebx, ebx
+loop4:
+	lodsw
+	add ebx, eax
+	loop loop4
+
+end:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, edi
+	int 0x80
+
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
